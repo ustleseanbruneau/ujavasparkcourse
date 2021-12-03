f6ef95ec54486f553963084ce79c509b415a1645
tb <tb@tb-VirtualBox>
2021-12-03 14:52:55 -0600

    Section 6 - PairRDDs Complete

--- ujavasparkcourse/src/main/java/tech/lesean/Main.java
+++ ujavasparkcourse/src/main/java/tech/lesean/Main.java
@@ -6,8 +6,9 @@ import java.util.List;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+
+import com.google.common.collect.Iterables;
 
 import scala.Tuple2;
 
@@ -15,11 +16,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		List<Integer> inputData = new ArrayList<>();
-		inputData.add(35);
-		inputData.add(12);
-		inputData.add(90);
-		inputData.add(20);
+		List<String> inputData = new ArrayList<>();
+		inputData.add("WARN: Tuesday 4 September 0405");
+		inputData.add("ERROR: Tuesday 4 September 0408");
+		inputData.add("FATAL: Wednesday 5 September 1632");
+		inputData.add("ERROR: Friday 7 September 1854");
+		inputData.add("WARN: Saturday 8 September 1942");
 		
  		// Get rid of extra logging
 		Logger.getLogger("org").setLevel(Level.ERROR);
@@ -28,16 +30,35 @@ public class Main {
 		SparkConf conf = new SparkConf().setAppName("First Spark Java Program").setMaster("local[*]");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		
-		JavaRDD<Integer> originalIntegers = sc.parallelize(inputData);
+		//JavaRDD<String> originalLogMessages = sc.parallelize(inputData);
 		
-		// First line - traditional Java with separate class
-		//JavaRDD<IntegerWithSquareRoot> sqrtRdd = originalIntegers.map( value -> new IntegerWithSquareRoot(value) );
-		// Second line - new Scale Tuple2 type - don't need separate Java class
-		JavaRDD<Tuple2<Integer, Double>> sqrtRdd = originalIntegers.map( value -> new Tuple2<>(value, Math.sqrt(value)) );
+		// split function on multiple lines
+//		JavaPairRDD<String, Long> pairRdd = originalLogMessages.mapToPair( rawValue -> {
+//			
+//			String[] columns = rawValue.split(":");
+//			String level = columns[0];
+//			
+//			return new Tuple2<>(level, 1L);
+//		} );
 		
-		// Examples of Tuples with different sizes
-		// new Tuple5(1, 4, 7, 3, 2)
-		// max size of Tuple = Tuple22
+		// JavaPairRDD<String, Long> pairRdd = originalLogMessages.mapToPair( rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L ));
+		
+		// JavaPairRDD<String, Long> sumsRdd = pairRdd.reduceByKey((value1, value2) -> value1 + value2);
+		
+		// sumsRdd.foreach( tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
+
+		// code optimization for the previous lines
+		sc.parallelize(inputData)
+			.mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L ))
+			.reduceByKey((value1, value2) -> value1 + value2)
+			.foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
+		
+		// groupbykey - careful in production environments
+//		sc.parallelize(inputData)
+//		.mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L ))
+//		.groupByKey()
+//		.foreach( tuple -> System.out.println(tuple._1 + " has " + Iterables.size(tuple._2) + " instances"));
+	
 		
 		sc.close();
 
