ce79302aede8aff9954d0765df02892458c5794e
tb <tb@tb-VirtualBox>
2021-12-08 05:10:24 -0600

    Section 9 - Keyword Ranking Practical - Complete

--- ujavasparkcourse/src/main/java/tech/lesean/Main.java
+++ ujavasparkcourse/src/main/java/tech/lesean/Main.java
@@ -1,14 +1,16 @@
 package tech.lesean;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
 
 public class Main {
 
@@ -25,11 +27,33 @@ public class Main {
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		
 		JavaRDD<String> initialRdd = sc.textFile("src/main/resources/subtitles/input.txt");
+		//JavaRDD<String> initialRdd = sc.textFile("src/main/resources/subtitles/input-spring.txt");
 		
-		// same as previous four lines - using consolodated coding
-		initialRdd
-			.flatMap(value -> Arrays.asList(value.split(" ")).iterator())
-			.foreach(value -> System.out.println(value));
+		// RegEx - [^a-zA-Z\\s] - if not a letter or space, replace
+		// all lowercase letters - for grouping and sorting
+		
+		JavaRDD<String> lettersOnlyRdd = initialRdd.map( sentence -> sentence.replaceAll("[^a-zA-Z\\s]", "").toLowerCase() );
+		
+		JavaRDD<String> removedBlankLines = lettersOnlyRdd.filter( sentence -> sentence.trim().length() > 0);
+
+		JavaRDD<String> justWords = removedBlankLines.flatMap( sentence -> Arrays.asList(sentence.split(" ")).iterator());
+		
+		JavaRDD<String> blankWordsRemoved = justWords.filter(word -> word.trim().length() > 0);
+		
+		JavaRDD<String> justInterestingWords = blankWordsRemoved.filter(word -> Util.isNotBoring(word));
+		
+		JavaPairRDD<String, Long> pairRdd = justInterestingWords.mapToPair(word -> new Tuple2<String, Long>(word, 1L));
+		
+		JavaPairRDD<String, Long> totals = pairRdd.reduceByKey((value1, value2) -> value1 + value2);
+		
+		JavaPairRDD<Long, String> switched = totals.mapToPair(tuple -> new Tuple2<Long, String> (tuple._2, tuple._1 ));
+		
+		JavaPairRDD<Long, String> sorted = switched.sortByKey(false);
+		
+		//List<String> results = justInterestingWords.take(50);
+		List<Tuple2<Long, String>> results = sorted.take(10);
+		
+		results.forEach(System.out::println);
 		
 		sc.close();
 
