7633d8f5c985fbadb817383c9a197d9a476f6d61
tb <tb@tb-VirtualBox>
2021-12-07 20:59:12 -0600

    Section 8 - Reading from  Disk - Complete

--- ujavasparkcourse/src/main/java/tech/lesean/Main.java
+++ ujavasparkcourse/src/main/java/tech/lesean/Main.java
@@ -14,12 +14,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		List<String> inputData = new ArrayList<>();
-		inputData.add("WARN: Tuesday 4 September 0405");
-		inputData.add("ERROR: Tuesday 4 September 0408");
-		inputData.add("FATAL: Wednesday 5 September 1632");
-		inputData.add("ERROR: Friday 7 September 1854");
-		inputData.add("WARN: Saturday 8 September 1942");
+		// Windows machines
+		//System.setProperty("hadoop.home.dir", "c:/opt/hadoop");
 		
  		// Get rid of extra logging
 		Logger.getLogger("org").setLevel(Level.ERROR);
@@ -28,18 +24,11 @@ public class Main {
 		SparkConf conf = new SparkConf().setAppName("First Spark Java Program").setMaster("local[*]");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		
-//		JavaRDD<String> sentences = sc.parallelize(inputData);
-//		
-//		JavaRDD<String> words = sentences.flatMap(value -> Arrays.asList(value.split(" ")).iterator());
-//		
-//		JavaRDD filteredWords = words.filter(word -> word.length() > 1);
-//		
-//		filteredWords.foreach(value -> System.out.println(value));
+		JavaRDD<String> initialRdd = sc.textFile("src/main/resources/subtitles/input.txt");
 		
 		// same as previous four lines - using consolodated coding
-		sc.parallelize(inputData)
+		initialRdd
 			.flatMap(value -> Arrays.asList(value.split(" ")).iterator())
-			.filter(word -> word.length() > 1)
 			.foreach(value -> System.out.println(value));
 		
 		sc.close();
