0fdbd02c0ff6cd7c4cf7f50a92bc23794dc1fdb5
tb <tb@tb-VirtualBox>
2021-12-17 12:53:09 -0600

    Section 20 - Grouping and Aggregations - Completed

--- ujavasparkcourse/src/main/java/tech/lesean/Main.java
+++ ujavasparkcourse/src/main/java/tech/lesean/Main.java
@@ -30,28 +30,30 @@ public class Main {
 				.getOrCreate();
 		
 
-		// Section 19 - In Memory Data
-		
+		// Section 20 - Grouping and Aggregations
 		List<Row> inMemory = new ArrayList<Row>();
-		
+
 		inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
 		inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
 		inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
 		inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
 		inMemory.add(RowFactory.create("FATAL","2015-4-21 19:23:20"));
-		
+
 		StructField[] fields = new StructField[] {
 			new StructField("level", DataTypes.StringType, false, Metadata.empty()),
 			new StructField("datetime", DataTypes.StringType, false, Metadata.empty())
 		};
-		
+
 		StructType schema = new StructType(fields);
-		
 		Dataset<Row> dataset = spark.createDataFrame(inMemory, schema);
 		
-		dataset.show();
+		dataset.createOrReplaceTempView("logging_table");
 		
-		
+		//Dataset<Row> results = spark.sql("select level, count(datetime) from logging_table group by level order by level");
+		Dataset<Row> results = spark.sql("select level, collect_list(datetime) from logging_table group by level order by level");
+
+		results.show();
+
 		spark.close();
 				
 		
