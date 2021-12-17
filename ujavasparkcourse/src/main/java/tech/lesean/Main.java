db9d710463111a0a2b5ecb9de960870763673343
tb <tb@tb-VirtualBox>
2021-12-17 06:48:23 -0600

    Section 16 - SparkSQL Getting Started - Complete

--- ujavasparkcourse/src/main/java/tech/lesean/Main.java
+++ ujavasparkcourse/src/main/java/tech/lesean/Main.java
@@ -1,16 +1,10 @@
 package tech.lesean;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.Optional;
-
-import scala.Tuple2;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 public class Main {
 
@@ -23,42 +17,29 @@ public class Main {
 		Logger.getLogger("org").setLevel(Level.ERROR);
 		Logger.getLogger("akka").setLevel(Level.ERROR);
 
-		SparkConf conf = new SparkConf().setAppName("First Spark Java Program").setMaster("local[*]");
-		JavaSparkContext sc = new JavaSparkContext(conf);
+		//  JavaRDD Setting up context
+		//SparkConf conf = new SparkConf().setAppName("First Spark Java Program").setMaster("local[*]");
+		//JavaSparkContext sc = new JavaSparkContext(conf);
 		
-		List<Tuple2<Integer, Integer>> visitsRaw = new ArrayList<>();
-		visitsRaw.add(new Tuple2<>(4,18));
-		visitsRaw.add(new Tuple2<>(6,4));
-		visitsRaw.add(new Tuple2<>(10,9));
+		// Java Spark - setting up a session
+		SparkSession spark = SparkSession.builder().appName("testingSql").master("local[*]")
+				// if on Windows - need to add the following to SparkSession command				
+				//.config("spark.sql.warehouse.dir","file:///c:/tmp/")
+				.getOrCreate();
 		
-		List<Tuple2<Integer, String>> usersRaw = new ArrayList<>();
-		usersRaw.add(new Tuple2<>(1, "John"));
-		usersRaw.add(new Tuple2<>(2, "Bob"));
-		usersRaw.add(new Tuple2<>(3, "Alan"));
-		usersRaw.add(new Tuple2<>(4, "Doris"));
-		usersRaw.add(new Tuple2<>(5, "Marybelle"));
-		usersRaw.add(new Tuple2<>(6, "Raquel"));
+		Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
 		
-		JavaPairRDD<Integer, Integer> visits = sc.parallelizePairs(visitsRaw);
-		JavaPairRDD<Integer, String> users = sc.parallelizePairs(usersRaw);
+		// show() will print out first 20 rows of dataset
+		dataset.show();
 		
-		// Left outer join
-		JavaPairRDD<Integer, Tuple2<Integer, Optional<String>>> joinedRdd = visits.leftOuterJoin(users);
-		// .orElse() - default a value if not exists
-		joinedRdd.foreach( value -> System.out.println(value) );
-		//joinedRdd.foreach( it -> System.out.println(it._2._2.orElse("blank").toUpperCase() ));
-
-		// right outer join
-		//JavaPairRDD<Integer, Tuple2<Optional<Integer>, String>> joinedRdd = visits.rightOuterJoin(users);
-		//joinedRdd.foreach( it -> System.out.println(" user " + it._2._2 + " had " + it._2._1.orElse(0) ));
-
-		// cartesian
-		//JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Integer, String>> joinedRdd = visits.cartesian(users);
-		//joinedRdd.foreach( value -> System.out.println(value) );
-
+		long numberOfRows = dataset.count();
+		System.out.println("There are " + numberOfRows + " records.");
 		
-		sc.close();
-
+		spark.close();
+				
+		
+		
+		
 	}
 
 }
