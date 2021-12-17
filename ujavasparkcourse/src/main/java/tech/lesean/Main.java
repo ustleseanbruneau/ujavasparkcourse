c636b0598f4bbd67f0574fa13b1b732e09e91cf4
tb <tb@tb-VirtualBox>
2021-12-17 10:10:09 -0600

    Section 19 - In Memory Data - Completed

--- ujavasparkcourse/src/main/java/tech/lesean/Main.java
+++ ujavasparkcourse/src/main/java/tech/lesean/Main.java
@@ -1,12 +1,18 @@
 package tech.lesean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
-import static org.apache.spark.sql.functions.*;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 public class Main {
 
@@ -23,27 +29,28 @@ public class Main {
 		SparkSession spark = SparkSession.builder().appName("testingSql").master("local[*]")
 				.getOrCreate();
 		
-		Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
+
+		// Section 19 - In Memory Data
 		
-		// Section 18 - The Full SQL Syntax
-		// SQL statement does not work in filter method
-		//Dataset<Row> modernArtResults = dataset.filter("select * from students where subject = 'Modern Art' AND year >= 2007 ");
-		//modernArtResults.show();
+		List<Row> inMemory = new ArrayList<Row>();
 		
-		// temporary view - in memory data structure
-		dataset.createOrReplaceTempView("my_students_table");
+		inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
+		inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
+		inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
+		inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
+		inMemory.add(RowFactory.create("FATAL","2015-4-21 19:23:20"));
 		
-		//Dataset<Row> results = spark.sql("select * from my_students_table where subject = 'French' AND year >= 2007 ");
-		//Dataset<Row> results = spark.sql("select score,year from my_students_table where subject = 'French' AND year >= 2007 ");
-		//Dataset<Row> results = spark.sql("select min(score) from my_students_table where subject = 'French' AND year >= 2007 ");
-		//Dataset<Row> results = spark.sql("select avg(score) from my_students_table where subject = 'French' AND year >= 2007 ");
-		//Dataset<Row> results = spark.sql("select max(score) from my_students_table where subject = 'French' AND year >= 2007 ");
+		StructField[] fields = new StructField[] {
+			new StructField("level", DataTypes.StringType, false, Metadata.empty()),
+			new StructField("datetime", DataTypes.StringType, false, Metadata.empty())
+		};
 		
-		//Dataset<Row> results = spark.sql("select distinct(year) from my_students_table ");
-		//Dataset<Row> results = spark.sql("select distinct(year) from my_students_table order by year");
-		Dataset<Row> results = spark.sql("select distinct(year) from my_students_table order by year desc");
+		StructType schema = new StructType(fields);
 		
-		results.show();
+		Dataset<Row> dataset = spark.createDataFrame(inMemory, schema);
+		
+		dataset.show();
+		
 		
 		spark.close();
 				
